9407522efa03ba19e01835940e9600756a08308b
Napoleon Vuong <napolo101@gmail.com>
2020-07-10 15:20:08 -0700

    Allowed for universal pokemon searching and implemented nes.css styling

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const submitBtn = document.querySelector(`.submitBtn`);
 $(document).ready(function () {
     $(submitBtn).on("click", (event) => {
         event.preventDefault();
-        var pokemon = "";
-        pokemon = $("#userInput").val();
+        let pokemon = $("#userInput").val();
         $("#userInput").val("");
         $.ajax({
             type: "GET",
-            url: "https://pokeapi.co/api/v2/pokemon/charizard/ ",
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/ `,
             datatype: "JSON",
         })
             .then((response) => {
@@ -23,10 +22,35 @@ $(document).ready(function () {
                 pokeType.textContent = `Type: ${response.types[0].type.name}`;
                 //Image
                 pokeImage.src = response.sprites.front_default;
-
             })
     })
 });
 
+////////////////////////////////////////////////////////////////////////////////////////////////TESTING
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
 
 //module.exports = { pokeName };
