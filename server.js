a8edff32f1e322f040c53777daa91a395ae48217
Napoleon Vuong <napolo101@gmail.com>
2020-07-13 21:18:34 -0700

    Yucky code

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// const express = require("express");
-// const session = require("express-session");
-// const passport = require("./config/passport.js");
-// const db = require("./models");
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport.js");
+const db = require("./models");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// require("dotenv").config();
+require("dotenv").config();
 
-// // Configure express input / output
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("./client"));
+// Configure express input / output
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./client"));
 
