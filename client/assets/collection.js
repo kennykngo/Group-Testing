1d9c537cbe05aa07a3c80352abe4f4454a353f76
Napoleon Vuong <napolo101@gmail.com>
2020-07-18 14:04:07 -0700

    Found targeting for Collections

--- client/assets/collection.js
+++ client/assets/collection.js
@@ -1,20 +1,44 @@
+const submitBtn = document.querySelector(`#submitBtn`);
+
 $(document).ready(function () {
-  const createLog = () => {
+
+  $("#logForm").on("submit", (e) => {
+    e.preventDefault();
+    let pokemon = $("#nameInput").val();
+    $("#nameInput").val("");
+    $.ajax({
+      type: "GET",
+      url: `https://pokeapi.co/api/v2/pokemon/${pokemon}/ `,
+      datatype: "JSON",
+    }).then((response) => {
+      console.log(response);
+      //Name
+
+      createLog(response.name)
+        .then(() => renderLogs());
+
+
+      //Type
+      //console.log(response.types[0].type.name);
+      // pokeType.textContent = `Type: ${response.types[0].type.name}`;
+      //Image
+      // pokeImage.src = response.sprites.front_default;
+    });
+
+  });
+
+  const createLog = (name) => {
     return new Promise((resolve, reject) => {
       $.ajax({
         type: "POST",
         url: "/logs/new",
         data: {
-          // company: $("#companyInput").val().trim(),
-          // roast: $("#roastInput").val().trim(),
-          // description: $("#descriptionInput").val().trim(),
-          name: $("#nameInput").val().trim(),
+          name: name,
+          // image: image,
+          // type: type,
         },
       }).then((res) => {
         console.log(res);
-        // $("#companyInput").val("");
-        // $("#roastInput").val("");
-        // $("#descriptionInput").val("");
         $("#nameInput").val("");
         logInstance.close();
         resolve("success");
@@ -39,21 +63,31 @@ $(document).ready(function () {
             name = "";
           }
           console.log(name);
-          $("#logsContainer").append(`
-          <div class="row">
-            <div class="card brown darken-1">
-              <div class="card-content white-text">
-              ${name}
-              </div>
+          // $("#logsContainer").append(`
+          // <div class="row">
+          //   <div class="card brown darken-1">
+          //     <div class="card-content white-text Pokemon-name"> 
+          //     ${name}
+          //     </div>
+          //   </div>
+          // </div>
+          // `);
+
+          $("#recent-cards").append(`
+                  <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">${name}<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">${name}<i class="material-icons right">close</i></span>
+              <p></p>
             </div>
           </div>
-          `);
-          // resolve("success");
+                  `)
         });
-        // <span class="card-title">${company}</span>
-        // <br>
-        // <p>${roast}</p>
-        // <p>${description}</p>
         resolve("success");
       });
     });
@@ -70,8 +104,5 @@ $(document).ready(function () {
   $("#newLogBtn").on("click", () => logInstance.open());
   $("#logCancel").on("click", () => logInstance.close());
 
-  $("#logForm").on("submit", (e) => {
-    e.preventDefault();
-    createLog().then(() => renderLogs());
-  });
+
 });
